a7d6beb8e1af2874a7dfe60453d1aa003eee4cad
Ashish M J <ashm.jagadeesh@gmail.com>
2021-03-03 16:10:47 +0530

    Update Files

---
+++ src/com/rakuten/test/Calculator.java
@@ -0,0 +1,17 @@
+package com.rakuten.test;
+
+public class Calculator {
+	
+	
+	int add(int a, int b) {
+		//business logic
+		return a+b;
+	}
+	
+	int multiply(int a, int b) {
+		//business logic
+		return a*b;
+	}
+
+	
+}
